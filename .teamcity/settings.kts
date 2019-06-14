a5240a51e0c1c9fdc3c070be1ec564d7a828c559
Anton Arhipov <ant.arhipov@gmail.com>
2019-06-14 13:34:45 +0300

    version bump

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.project
 import jetbrains.buildServer.configs.kotlin.v2018_2.version
 
-version = "2018.1"
+version = "2019.1"
 
 project {
     buildType(Build)
